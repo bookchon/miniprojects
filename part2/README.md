2d99cec597b38aeb901d2865dac095d8737c9217
bookchon <realtansan@gmail.com>
2023-05-03 09:05:04 +0900

    2023. 5. 2.

---
+++ part2/README.md
@@ -0,0 +1,13 @@
+# 미니프로젝트 Part2
+기간 - 2023. 5. 2. ~ 6. 16.
+
+## C# WPF 학습
+- SCADA 시뮬레이션
+	- C# WPF 
+	- MahApps.Metro (MetroUI 디장니 라이브러리)
+	- Bogus (더미데이터 생성 라이브러리)
+	- Mewtonsofr.json
+	- M2Mqtt(통신 라이브러리)
+	- DB 데이터 바인딩
+	- LiveCharts
+	- OxyChart
---
+++ part2/studySCADA/ScadaSimulation/BogusTestApps/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogusTestApps.Models
+{
+    public class Customer // 고객 테이블 매핑
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string ContactName { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
+
+    }
+}
